e08eff0df92f1fdabaabcfe310467ae47e3d647d
Serge Fedosov <107842308+serge-fedosov@users.noreply.github.com>
2022-09-11 18:19:00 +0400

    Simple Chatty Bot - Stage 3/5

--- Simple Chatty Bot/Simple Chatty Bot/task/main.js
+++ Simple Chatty Bot/Simple Chatty Bot/task/main.js
@@ -7,3 +7,12 @@ console.log("Please, remind me your name.");
 let name = input();
 
 console.log("What a great name you have, " + name + "!");
+console.log("Let me guess your age.");
+console.log("Enter remainders of dividing your age by 3, 5 and 7.");
+
+let remainder3 = input();
+let remainder5 = input();
+let remainder7 = input();
+let age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105;
+
+console.log("Your age is " + age + "; that's a good time to start programming!");
