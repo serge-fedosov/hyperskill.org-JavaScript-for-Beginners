8ee4125b78bad4844f3773404f73d0d156b07962
Serge Fedosov <107842308+serge-fedosov@users.noreply.github.com>
2022-09-11 18:06:29 +0400

    Simple Chatty Bot - Stage 2/5

--- Simple Chatty Bot/Simple Chatty Bot/task/main.js
+++ Simple Chatty Bot/Simple Chatty Bot/task/main.js
@@ -1,2 +1,9 @@
+const input = require('sync-input');
+
 console.log("Hello! My name is Aid.");
 console.log("I was created in 2022.");
+console.log("Please, remind me your name.");
+
+let name = input();
+
+console.log("What a great name you have, " + name + "!");
