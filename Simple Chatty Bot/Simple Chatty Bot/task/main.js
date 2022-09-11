8a19037974bfee9a84f59be8d3d0d195cd918f75
Serge Fedosov <107842308+serge-fedosov@users.noreply.github.com>
2022-09-11 19:12:22 +0400

    Simple Chatty Bot - Stage 4/5

--- Simple Chatty Bot/Simple Chatty Bot/task/main.js
+++ Simple Chatty Bot/Simple Chatty Bot/task/main.js
@@ -1,18 +1,29 @@
 const input = require('sync-input');
 
-console.log("Hello! My name is Aid.");
-console.log("I was created in 2022.");
-console.log("Please, remind me your name.");
+console.log("Hello! My name is Aid.")
+console.log("I was created in 2020.")
+console.log("Please, remind me your name.")
 
-let name = input();
+let name = input()
 
-console.log("What a great name you have, " + name + "!");
-console.log("Let me guess your age.");
-console.log("Enter remainders of dividing your age by 3, 5 and 7.");
+console.log("What a great name you have, " + name + "!")
+console.log("Let me guess your age.")
+console.log("Enter remainders of dividing your age by 3, 5 and 7.")
 
-let remainder3 = input();
-let remainder5 = input();
-let remainder7 = input();
-let age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105;
+let rem3 = Number(input());
+let rem5 = Number(input());
+let rem7 = Number(input());
+
+let age = (rem3 * 70 + rem5 * 21 + rem7 * 15) % 105;
 
 console.log("Your age is " + age + "; that's a good time to start programming!");
+console.log("Now I will prove to you that I can count to any number you want.");
+
+let num = Number(input());
+let count = 0;
+while (count <= num) {
+    console.log(count + " !");
+    count++;
+}
+
+console.log("Completed, have a nice day!");
---
+++ Simple Chatty Bot/Topics/While Loops/A mistake/main.js
@@ -0,0 +1,5 @@
+let n = 5;
+while (n < 10) {
+    n = n + 1;
+    console.log(n);
+}
