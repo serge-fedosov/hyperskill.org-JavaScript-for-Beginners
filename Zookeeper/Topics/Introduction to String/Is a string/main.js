a14fcf60ce9612e60537e9943a2ca252fe9903c0
Serge Fedosov <107842308+serge-fedosov@users.noreply.github.com>
2022-09-12 20:43:01 +0400

    Zookeeper - Stage 2/4

---
+++ Zookeeper/Topics/Introduction to String/Is a string/main.js
@@ -0,0 +1,10 @@
+function isString(data) {
+    if (typeof data === 'string') {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+console.log(isString("this is a string"));
+console.log(isString("test code"));
