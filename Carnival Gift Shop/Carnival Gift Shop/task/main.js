f815a09434f95d6a4327188e238d8110dff5f402
Serge Fedosov <107842308+serge-fedosov@users.noreply.github.com>
2022-09-13 17:36:40 +0400

    Carnival Gift Shop - Stage 2/5

--- Carnival Gift Shop/Carnival Gift Shop/task/main.js
+++ Carnival Gift Shop/Carnival Gift Shop/task/main.js
@@ -2,14 +2,13 @@ console.log("WELCOME TO THE CARNIVAL GIFT SHOP!");
 console.log("Hello friend! Thank you for visiting the carnival!");
 
 console.log("Here's the list of gifts:\n");
-
-console.log(`Teddy Bear
-Big Red Ball
-Huge Bear
-Candy
-Stuffed Tiger
-Stuffed Dragon
-Skateboard
-Toy Car
-Basketball
-Scary Mask`);
+console.log(`1- Teddy Bear, Cost: 10 tickets
+2- Big Red Ball, Cost: 5 tickets
+3- Huge Bear, Cost: 50 tickets
+4- Candy, Cost: 8 tickets
+5- Stuffed Tiger, Cost: 15 tickets
+6- Stuffed Dragon, Cost: 30 tickets
+7- Skateboard, Cost: 100 tickets
+8- Toy Car, Cost: 25 tickets
+9- Basketball, Cost: 20 tickets
+10- Scary Mask, Cost: 75 tickets`);
