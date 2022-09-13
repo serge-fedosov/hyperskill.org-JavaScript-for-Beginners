af4d17329f937c63800feb3369add7f6675990b3
Serge Fedosov <107842308+serge-fedosov@users.noreply.github.com>
2022-09-13 16:41:52 +0400

    Simple Currency Converter - Stage 3/4

--- Simple Currency Converter/Simple Currency Converter/task/main.js
+++ Simple Currency Converter/Simple Currency Converter/task/main.js
@@ -1,4 +1,6 @@
 const input = require('sync-input');
+const currencies = ["JPY", "EUR", "RUB", "USD", "GBP"];
+const exchangeRate = [113.5, 0.89, 74.36, 1, 0.75];
 
 console.log("Welcome to Currency Converter!");
 console.log(`1 USD equals 1 USD
@@ -6,23 +8,25 @@ console.log(`1 USD equals 1 USD
 1 USD equals 0.89 EUR
 1 USD equals 74.36 RUB
 1 USD equals 0.75 GBP`);
-console.log("I can convert USD to these currencies: JPY, EUR, RUB, USD, GBP");
 
-console.log("Type the currency you wish to convert: USD");
-const currencies = ["JPY", "EUR", "RUB", "USD", "GBP"];
-const exchangeRate = [113.5, 0.89, 74.36, 1, 0.75];
-let currency = input("To: ").toUpperCase();
-
-if (currencies.includes(currency)) {
-    let amount = Number(input("Amount: "));
-    if (isNaN(amount)) {
-        console.log("The amount has to be a number");
-    } else if (amount < 1) {
-        console.log("The amount cannot be less than 1");
+console.log("What do you want to convert?");
+let fromCurr = input("From: ").toUpperCase();
+if (currencies.includes(fromCurr)) {
+    let toCurr = input("To: ").toUpperCase();
+    if (currencies.includes(toCurr)) {
+        let amount = Number(input("Amount: "));
+        if (isNaN(amount)) {
+            console.log("The amount has to be a number");
+        } else if (amount < 1) {
+            console.log("The amount cannot be less than 1");
+        } else {
+            const fromIndex = currencies.indexOf(fromCurr);
+            const toIndex = currencies.indexOf(toCurr);
+            const value = amount / exchangeRate[fromIndex] * exchangeRate[toIndex];
+            console.log(`Result: ${amount} ${currencies[fromIndex]} equals ${value.toFixed(4)} ${currencies[toIndex]}`);
+        }
     } else {
-        const i = currencies.indexOf(currency);
-        const value = amount * exchangeRate[i];
-        console.log(`Result: ${amount} USD equals ${value.toFixed(4)} ${currencies[i]}`);
+        console.log("Unknown currency");
     }
 } else {
     console.log("Unknown currency");
