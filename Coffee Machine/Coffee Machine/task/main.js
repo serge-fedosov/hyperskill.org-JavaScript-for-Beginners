3de829f1effe1b4cf78f69e9beede4d4bfee58a5
Serge Fedosov <107842308+serge-fedosov@users.noreply.github.com>
2022-09-14 11:05:53 +0400

    Coffee Machine - Stage 2/6

--- Coffee Machine/Coffee Machine/task/main.js
+++ Coffee Machine/Coffee Machine/task/main.js
@@ -3,10 +3,22 @@
 // You will need this in the following stages
 const input = require('sync-input')
 
-console.log(`Starting to make a coffee
-Grinding coffee beans
-Boiling water
-Mixing boiled water with crushed coffee beans
-Pouring coffee into the cup
-Pouring some milk into the cup
-Coffee is ready!`);
+// console.log(`Starting to make a coffee
+// Grinding coffee beans
+// Boiling water
+// Mixing boiled water with crushed coffee beans
+// Pouring coffee into the cup
+// Pouring some milk into the cup
+// Coffee is ready!`);
+
+console.log("Write how many cups of coffee you will need:");
+let cups = Number(input());
+let water = cups * 200;
+let milk = cups * 50;
+let coffee = cups * 15;
+
+console.log(`For ${cups} cups of coffee you will need:
+${water} ml of water
+${milk} ml of milk
+${coffee} g of coffee beans`);
+
