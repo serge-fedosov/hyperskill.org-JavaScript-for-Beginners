2b2bf6facf59e150ff0e0d95a702848320dea9a1
Serge Fedosov <107842308+serge-fedosov@users.noreply.github.com>
2022-09-14 11:23:13 +0400

    Coffee Machine - Stage 4/6

--- Coffee Machine/Coffee Machine/task/main.js
+++ Coffee Machine/Coffee Machine/task/main.js
@@ -1,24 +1,26 @@
-// Use "input()" to input a line from the user
-// Use "input(str)" to print some text before requesting input
-// You will need this in the following stages
 const input = require('sync-input')
 
-// console.log(`Starting to make a coffee
-// Grinding coffee beans
-// Boiling water
-// Mixing boiled water with crushed coffee beans
-// Pouring coffee into the cup
-// Pouring some milk into the cup
-// Coffee is ready!`);
+console.log("Write how many ml of water the coffee machine has:");
+let hasWater = Number(input());
+
+console.log("Write how many ml of milk the coffee machine has:");
+let hasMilk = Number(input());
+
+console.log("Write how many grams of coffee beans the coffee machine has:");
+let hasCoffee = Number(input());
 
 console.log("Write how many cups of coffee you will need:");
-let cups = Number(input());
-let water = cups * 200;
-let milk = cups * 50;
-let coffee = cups * 15;
+let needCups = Number(input());
 
-console.log(`For ${cups} cups of coffee you will need:
-${water} ml of water
-${milk} ml of milk
-${coffee} g of coffee beans`);
+let water = Math.floor(hasWater / 200);
+let milk = Math.floor(hasMilk / 50);
+let coffee = Math.floor(hasCoffee / 15);
+let cups = Math.min(water, milk, coffee);
 
+if (cups < needCups) {
+    console.log(`No, I can make only ${cups} cups of coffee`);
+} else if (cups === needCups) {
+    console.log("Yes, I can make that amount of coffee");
+} else {
+    console.log(`Yes, I can make that amount of coffee (and even ${cups - needCups} more than that)`);
+}
